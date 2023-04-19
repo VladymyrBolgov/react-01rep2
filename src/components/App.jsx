a2a806aea62f0751f7376a257cd305844e707328
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-04-19 23:46:13 +0300

    src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
-import { PageTitle } from 'components/PageTitle/PageTitle';
-import { EventBoard } from 'components/EventBoard/EventBoard';
-import upcomingEvents from '../upcoming-events.json';
-
 export const App = () => {
   return (
-    <>
-      <PageTitle text="24th Core Worlds Coalition Conference" />
-      <EventBoard events={upcomingEvents} />
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
