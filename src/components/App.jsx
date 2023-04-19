0b75ce6d30b39a1d7380f2df0689429636d9b142
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-04-20 00:02:52 +0300

    src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
-import { EventBoard } from 'components/EventBoard/EventBoard';
-import upcomingEvents from '../upcoming-events.json';
+// import { EventBoard } from 'components/EventBoard/EventBoard';
+// import upcomingEvents from '../upcoming-events.json';
 
 export const App = () => {
   return (
     <>
       <PageTitle text="24th Core Worlds Coalition Conference" />
-      <EventBoard events={upcomingEvents} />
+      {/* <EventBoard events={upcomingEvents} /> */}
     </>
   );
 };
